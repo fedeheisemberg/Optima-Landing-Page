c81cb73a20b98931f9ff0050b2943ee944859d42
Federico <fedemarti0107@gmail.com>
2024-04-03 01:43:03 -0300

    brochure added

--- components/about-us/index.js
+++ components/about-us/index.js
@@ -10,23 +10,30 @@ const AboutUs = () => {
             Nuestros Servicios
           </h2>
           <p className="mt-4 text-lg leading-6 text-gray-600">
-          Nos especializamos en estrategias de inversión y aplicaciones de machine learning orientadas al ámbito corporativo, con el fin de potenciar resultados y acrecentar la rentabilidad. 
+            Nos especializamos en estrategias de inversión y aplicaciones de machine learning orientadas al ámbito corporativo, con el fin de potenciar resultados y acrecentar la rentabilidad.
           </p>
           <p className="mt-2 text-lg leading-6 text-gray-600">
             Somos un equipo de profesionales de las Ciencias Económicas y de la
             Computación específicamente dedicado a ayudarte a mejorar tu
             situación financiera de largo plazo, ofreciendo además soluciones personalizadas de aprendizaje automático que catalizan la eficiencia operativa y la transformación empresarial.
           </p>
-          <button
-            type="button"
-            className="mt-8 inline-flex items-center px-6 py-3 border border-transparent text-lg leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition ease-in-out duration-150"
+          <a 
+            href="https://drive.google.com/file/d/1ffc2_KmQjQW-OUMgsRKG77nghW-qPL8J/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            Quiero saber más
-          </button>
+            <button
+              type="button"
+              className="mt-8 inline-flex items-center px-6 py-3 border border-transparent text-lg leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition ease-in-out duration-150"
+            >
+              Quiero saber más
+            </button>
+          </a>
         </div>
       </div>
     </section>
   );
 };
+
 
 export default AboutUs;
