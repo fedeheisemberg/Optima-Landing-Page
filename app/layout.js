8fcf7b80b258531bd6980870e5b4b906f9ca1a25
maticamisay <maticamisay99@hotmail.com>
2024-05-08 00:08:01 -0300

    ADD google tag code

--- app/layout.js
+++ app/layout.js
@@ -3,17 +3,21 @@ import "./globals.css";
 import "./old.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+// import GoogleAnalytics from "./google-analytics"; // DESCOMENTAR PARA ACTIVAR GOOGLE ANALYTICS
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Optima Consulting & Management",
-  description: "Somos una empresa de Data Science aplicado a Finanzas Empresariales.",
+  description:
+    "Somos una empresa de Data Science aplicado a Finanzas Empresariales.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-200`}>
+        {/* <GoogleAnalytics /> */}{" "}
+        {/* DESCOMENTAR PARA ACTIVAR GOOGLE ANALYTICS */}
         <ToastContainer />
         {children}
       </body>
