0a29c406cccdc40acafedb6aae0f9bff0d93fbd8
maticamisay <maticamisay99@hotmail.com>
2024-05-08 01:43:13 -0300

    FIX login

--- src/app/signin/_action.js
+++ src/app/signin/_action.js
@@ -2,6 +2,7 @@
 import { redirect } from "next/navigation";
 import { parseResponse } from "@/libs/parse";
 import { signIn } from "../../auth";
+import { isRedirectError } from "next/dist/client/components/redirect";
 
 export default async function signInAction(credentials) {
   let success = false;
@@ -9,16 +10,18 @@ export default async function signInAction(credentials) {
     await signIn("credentials", {
       ...credentials,
       callbackUrl: "/admin",
-      redirect: false,
+      redirect: true,
     });
     success = true;
   } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
     console.log("in signinAction");
     console.log({ error });
     return { error: parseResponse(error.message) };
-  } finally {
-    if (success) {
-      redirect("/admin");
-    }
+  }
+  if (success) {
+    redirect("/admin");
   }
 }
